6d6729e22492642e84afa34e9b2977777c86059b
Fedia23 <fedia.ch@gmail.com>
2019-05-24 10:58:51 +0300

    authentication users

--- src/main/java/com/thymeleaf/course/domain/model/dto/UserSignUpRequest.java
+++ src/main/java/com/thymeleaf/course/domain/model/dto/UserSignUpRequest.java
@@ -3,15 +3,37 @@ package com.thymeleaf.course.domain.model.dto;
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.experimental.FieldDefaults;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Data
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class UserSignUpRequest {
 
+    @NotBlank
+    @Size(min = 3, max = 10, message = "First name invalid")
     String firstName;
-    String lastName;
-    String email;
-    String password;
-    String confirmPassword;
 
+    @NotBlank
+    @Size(min = 3, max = 10, message = "Last name invalid")
+    String lastName;
+
+    @NotBlank
+    @Size(min = 3, max = 10, message = "User name invalid")
+    String username;
+
+    @NotBlank
+    @Email
+    String email;
+
+    @NotBlank
+    @Size(min = 5, max = 35, message = "Password invalid")
+    String password;
+
+    @NotBlank
+    String confirmPassword;
 }
