46093516cfc3e001e7818431055656a74385d530
Liliana_Chevka <lilisla83@gmail.com>
2020-10-22 00:31:17 +0300

    Changes in some files

--- src/main/java/com/thymeleaf/course/controller/UserController.java
+++ src/main/java/com/thymeleaf/course/controller/UserController.java
@@ -1,9 +1,16 @@
 package com.thymeleaf.course.controller;
 
+import com.thymeleaf.course.domain.model.dto.UserSignUpRequest;
+import com.thymeleaf.course.domain.model.entity.User;
 import com.thymeleaf.course.domain.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping(value = "/users")
@@ -12,4 +19,22 @@ public class UserController {
 
     final UserService userService;
 
+    @GetMapping("/create")
+    public String signUpUser(UserSignUpRequest user){
+        return "registration";
+
+    }
+
+    @PostMapping("/create")
+    public String signUpNewUser(UserSignUpRequest user){
+        userService.saveUser(user);
+        return "greetingPage";
+    }
+
+    @GetMapping("/greeting")
+    public String sayHello(Model model){
+        List<User> users = userService.getUsersList();
+        model.addAttribute("greeting",users);
+        return "greetingPage";
+    }
 }
--- src/main/java/com/thymeleaf/course/domain/model/dto/EmployeeDto.java
+++ src/main/java/com/thymeleaf/course/domain/model/dto/EmployeeDto.java
@@ -33,4 +33,36 @@ public class EmployeeDto {
                 .concat(" ")
                 .concat(gender.toString());
     }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
 }
