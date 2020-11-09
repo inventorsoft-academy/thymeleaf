43cdf339fb685c8ad03a260c3505a45042a2f68e
Oleh Khalus <oleh0704@gmail.com>
2020-11-09 07:04:20 +0200

    thymleaf homework

--- src/main/java/com/thymeleaf/course/controller/UserController.java
+++ src/main/java/com/thymeleaf/course/controller/UserController.java
@@ -1,15 +1,47 @@
 package com.thymeleaf.course.controller;
 
+import com.thymeleaf.course.domain.model.dto.UserSignUpRequest;
+import com.thymeleaf.course.domain.model.entity.User;
 import com.thymeleaf.course.domain.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping(value = "/users")
 @AllArgsConstructor
 public class UserController {
 
-    final UserService userService;
+    UserService userService;
+
+    @GetMapping("/show")
+    public String showAllUsers(Model model) {
+        model.addAttribute("users", userService.getUserArrayList());
+        return "show-users";
+    }
+
+    @GetMapping("/create")
+    public String createUserForm(@ModelAttribute("user") UserSignUpRequest user) {
+        return "user-create";
+    }
+
+    @PostMapping("/create")
+    public String createUser(@ModelAttribute("user") UserSignUpRequest user) {
+        userService.saveUser(user);
+        return "redirect:/users/greeting";
+    }
+
+    @GetMapping("/greeting")
+    public String sayHelloTo(Model model) {
+        List<User> users = userService.getUserArrayList();
+        model.addAttribute("greetingToUser", users);
+        return "greeting";
+    }
 
 }
